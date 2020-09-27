7f88216dccebf302ea91218683e0bc43cc42815c
Nguyen Dinh Thang <llsogekingll@gmail.com>
2020-09-27 15:01:04 +1000

    GoNogo - add in test feature

--- app/src/main/java/com/federation/funf_test/gonogo/GoNogoInTestActivity.java
+++ app/src/main/java/com/federation/funf_test/gonogo/GoNogoInTestActivity.java
@@ -2,16 +2,23 @@ package com.federation.funf_test.gonogo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
 import com.federation.funf_test.R;
 
 public class GoNogoInTestActivity extends Activity {
+    Button intestBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.gonogo_intest);
+
+        intestBtn = (Button) findViewById(R.id.gonogoInTest_btn);
+
+
     }
 }
