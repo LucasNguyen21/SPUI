a5321dd200968ce2d412bb5a95ec9cd950004af5
Nguyen Dinh Thang <llsogekingll@gmail.com>
2020-10-07 23:11:32 +1100

    fix onBoot

--- app/src/main/java/Broadcaster/OnBootBroadcaster.java
+++ app/src/main/java/Broadcaster/OnBootBroadcaster.java
@@ -10,9 +10,7 @@ public class OnBootBroadcaster extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) || Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction())){
-            Intent toMainActivityIntent = new Intent(context, MainActivity.class);
-            toMainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(toMainActivityIntent);
+
         }
     }
 }
