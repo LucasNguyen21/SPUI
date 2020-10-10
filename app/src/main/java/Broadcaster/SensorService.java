2a2093a74ab81659eb32bafbe69f91f42c9a4acb
akaitran <tqa.khuong@gmail.com>
2020-10-11 00:02:36 +1100

    Update SensorService.java

--- app/src/main/java/Broadcaster/SensorService.java
+++ app/src/main/java/Broadcaster/SensorService.java
@@ -8,14 +8,19 @@ import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.hardware.SensorEventListener;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.federation.funf_test.JSONParser;
+
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -23,6 +28,11 @@ public class SensorService extends Service implements SensorEventListener {
     ArrayList<JSONObject> sensorList;
     float xAccel, yAccel, zAccel;
     float xLastAccel, yLastAccel, zLastAccel;
+
+    JSONParser jsonParser = new JSONParser();
+    private static String url_create = "https://pos.pentacle.tech/api/sensor/create";
+    private static final String TAG_SUCCESS = "success";
+    ArrayList params = new ArrayList();
 
     boolean firstUpdate = true;
     boolean shakeInitiated = false;
@@ -52,7 +62,6 @@ public class SensorService extends Service implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         updateAccelParameters(event.values[0], event.values[1], event.values[2]);
-        Log.d("Sensor", "x: " + event.values[0] + "y: " + event.values[1] + "z: " + event.values[2]);
         JSONObject sensor =  new JSONObject();
 
         try {
@@ -70,6 +79,18 @@ public class SensorService extends Service implements SensorEventListener {
             executeShakeAction();
         } else if (shakeInitiated && !isAccelerationChanged()) {
             shakeInitiated = false;
+        }
+
+        if (sensorList.size() == 100) {
+            Log.d("Sensor List", sensorList.toString());
+            String androidId = Settings.Secure.getString(getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+
+            params.add(new BasicNameValuePair("device_id", androidId));
+            params.add(new BasicNameValuePair("sensor_list", sensorList.toString()));
+
+            new CreateNewResult().execute();
+            sensorList = new ArrayList();
         }
     }
 
@@ -106,5 +127,36 @@ public class SensorService extends Service implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    class CreateNewResult extends AsyncTask<String, String, JSONObject> {
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * Creating product
+         */
+        protected JSONObject doInBackground(String... args) {
+            // getting JSON Object
+            // Note that create product url accepts POST methodN
+            JSONObject json = jsonParser.makeHttpRequest(url_create,
+                    "POST", params);
+            // check log cat fro response
+            Log.d("Debug", json.toString());
+
+
+            return json;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         **/
+        protected void onPostExecute(JSONObject file_url) {
+        }
     }
 }
