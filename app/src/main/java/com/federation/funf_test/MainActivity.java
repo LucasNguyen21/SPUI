1adc0e35c63afd13291fc6873d8e578a1012c5b1
akaitran <tqa.khuong@gmail.com>
2020-10-12 01:36:15 +1100

    Update MainActivity.java

--- app/src/main/java/com/federation/funf_test/MainActivity.java
+++ app/src/main/java/com/federation/funf_test/MainActivity.java
@@ -82,6 +82,9 @@ public class MainActivity extends AppCompatActivity {
             setSmsBroadcast();
 
             getAppUsageBackground();
+
+            Intent sensorIntent = new Intent(this, SensorService.class);
+            startService(sensorIntent);
         }
 
 
@@ -91,9 +94,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-//        Intent sensorIntent = new Intent(this, SensorService.class);
-//        startService(sensorIntent);
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -114,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
                 setSmsBroadcast();
 
                 getAppUsageBackground();
-
 
             } else {
                 Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
